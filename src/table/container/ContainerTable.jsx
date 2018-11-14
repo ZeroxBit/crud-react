6050d4297d06d94e17ac83456d61b51210489f46
Carlos <djkrlosmix1@gmail.com>
2018-11-13 22:55:32 -0500

    crup spa

---
+++ src/table/container/ContainerTable.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Table from '../components/Table';
+
+export class ContainerTable extends Component {
+  render() {
+    return (
+      <Table />
+    )
+  }
+}
+
+export default ContainerTable
